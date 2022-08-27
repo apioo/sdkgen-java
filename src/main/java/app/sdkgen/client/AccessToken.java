eb1c27e9c5ba80be0386c0c4eb3f8afe770dbca9
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-08-27 09:44:59 +0200

    add json property annotations

--- src/main/java/app/sdkgen/client/AccessToken.java
+++ src/main/java/app/sdkgen/client/AccessToken.java
@@ -1,5 +1,7 @@
 package app.sdkgen.client;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class AccessToken {
     private final String accessToken;
     private final String tokenType;
@@ -7,12 +9,20 @@ public class AccessToken {
     private final String refreshToken;
     private final String scope;
 
-    public AccessToken(String accessToken, String tokenType, int expiresIn, String refreshToken, String scope) {
+    public AccessToken(@JsonProperty("access_token") String accessToken, @JsonProperty("token_type") String tokenType, @JsonProperty("expires_in") int expiresIn, @JsonProperty("refresh_token") String refreshToken, @JsonProperty("scope") String scope) {
         this.accessToken = accessToken;
         this.tokenType = tokenType;
         this.expiresIn = expiresIn;
         this.refreshToken = refreshToken;
         this.scope = scope;
+    }
+
+    public AccessToken(@JsonProperty("access_token") String accessToken, @JsonProperty("token_type") String tokenType, @JsonProperty("expires_in") int expiresIn, @JsonProperty("refresh_token") String refreshToken) {
+        this(accessToken, tokenType, expiresIn, refreshToken, null);
+    }
+
+    public AccessToken(@JsonProperty("access_token") String accessToken, @JsonProperty("token_type") String tokenType, @JsonProperty("expires_in") int expiresIn) {
+        this(accessToken, tokenType, expiresIn, null, null);
     }
 
     public String getAccessToken() {
@@ -33,5 +43,9 @@ public class AccessToken {
 
     public String getScope() {
         return scope;
+    }
+
+    public boolean hasRefreshToken() {
+        return this.refreshToken != null && !this.refreshToken.isEmpty();
     }
 }
