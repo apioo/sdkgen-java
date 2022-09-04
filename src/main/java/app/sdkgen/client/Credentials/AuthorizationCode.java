a951f8a3f45f66e6d3fd779fe450a595403d96cf
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-09-04 20:49:40 +0200

    remove refresh url

--- src/main/java/app/sdkgen/client/Credentials/AuthorizationCode.java
+++ src/main/java/app/sdkgen/client/Credentials/AuthorizationCode.java
@@ -1,7 +1,7 @@
 package app.sdkgen.client.Credentials;
 
 public class AuthorizationCode extends OAuth2Abstract {
-    public AuthorizationCode(String clientId, String clientSecret, String tokenUrl, String authorizationUrl, String refreshUrl) {
-        super(clientId, clientSecret, tokenUrl, authorizationUrl, refreshUrl);
+    public AuthorizationCode(String clientId, String clientSecret, String tokenUrl, String authorizationUrl) {
+        super(clientId, clientSecret, tokenUrl, authorizationUrl);
     }
 }
