408e9e00816a2ff6d4c73da794c354544df24cbb
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-02-21 00:48:42 +0100

    add authenticator method

--- src/main/java/app/sdkgen/client/ClientAbstract.java
+++ src/main/java/app/sdkgen/client/ClientAbstract.java
@@ -5,19 +5,14 @@ import app.sdkgen.client.Credentials.Anonymous;
 import app.sdkgen.client.Exception.Authenticator.InvalidCredentialsException;
 import org.apache.http.client.HttpClient;
 
-import java.util.List;
-
 public abstract class ClientAbstract {
     public static final String USER_AGENT = "SDKgen Client v1.0";
-    protected String baseUrl;
-    protected CredentialsInterface credentials;
+
     protected AuthenticatorInterface authenticator;
-    protected List<String> scopes;
     protected HttpClient httpClient;
     protected Parser parser;
 
     public ClientAbstract(String baseUrl, CredentialsInterface credentials) throws InvalidCredentialsException {
-        this.baseUrl = baseUrl;
         this.authenticator = AuthenticatorFactory.factory(credentials);
         this.httpClient = (new HttpClientFactory(this.authenticator)).factory();
         this.parser = new Parser(baseUrl);
@@ -25,5 +20,9 @@ public abstract class ClientAbstract {
 
     public ClientAbstract(String baseUrl) throws InvalidCredentialsException {
         this(baseUrl, new Anonymous());
+    }
+
+    public AuthenticatorInterface getAuthenticator() {
+        return this.authenticator;
     }
 }
